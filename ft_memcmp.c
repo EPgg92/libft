d512d6d94e47366e1bb4468ebb213f32599f4bae
Enzo POGGIO <poggio.enzo@gmail.com>
2018-10-27 15:50:11 +0200

    safe incrementation

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -16,7 +16,11 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	while (n--)
-		if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
-			return (*(unsigned char *)(--s1) - *(unsigned char *)(--s2));
+	{
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)(s1) - *(unsigned char *)(s2));
+		s1++;
+		s2++;
+	}
 	return (0);
 }
