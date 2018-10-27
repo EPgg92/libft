6fe789fe32ec0235b631e1ee194a0eba60371bce
Enzo POGGIO <poggio.enzo@gmail.com>
2018-10-27 16:19:33 +0200

    safe incrementation

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -16,8 +16,12 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	while (n--)
-		if ((*(unsigned char *)dst++ = *(unsigned char *)src++)
+	{
+		if ((*(unsigned char *)dst = *(unsigned char *)src)
 			== (unsigned char)c)
 			return (dst);
+		dst++;
+		src++;
+	}
 	return (NULL);
 }
